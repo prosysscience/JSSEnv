84abdf5a18f01f89291425b5c8fb3ea720261421
Pierre Tassel <pierre.tassel@outlook.fr>
2021-01-19 13:50:14 +0100

    add manifest

--- setup.py
+++ setup.py
@@ -2,5 +2,6 @@ from setuptools import setup
 
 setup(name='JSSEnv',
       version='1.0.0',
-      install_requires=['gym', 'pandas', 'numpy', 'plotly']
+      install_requires=['gym', 'pandas', 'numpy', 'plotly'],
+      include_package_data=True
 )
