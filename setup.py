67bead035e6bc280c26333aa6d25943252a00c1d
Pierre TASSEL <pierre.tassel@outlook.fr>
2021-02-01 14:52:14 +0100

    Create pip package

--- setup.py
+++ setup.py
@@ -1,7 +1,19 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='JSSEnv',
       version='1.0.0',
-      install_requires=['gym', 'pandas', 'numpy', 'plotly', 'imageio', 'psutil', 'requests', 'kaleido', 'pytest', 'codecov'],
+      author="Pierre Tassel",
+      author_email="pierre.tassel@aau.at",
+      description="An optimized OpenAi gym's environment to simulate the Job-Shop Scheduling problem.",
+      url="https://github.com/prosysscience/JSSEnv",
+      packages=find_packages(),
+      classifiers=[
+          "Programming Language :: Python :: 3",
+          "License :: OSI Approved :: MIT License",
+          "Operating System :: OS Independent",
+      ],
+      python_requires='>=3.6',
+      install_requires=['gym', 'pandas', 'numpy', 'plotly', 'imageio', 'psutil', 'requests', 'kaleido', 'pytest',
+                        'codecov'],
       include_package_data=True
-)
+      )
